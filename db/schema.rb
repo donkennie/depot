bf62439fdd61438e0399cc3282371a887b2859fd
donkennie <ajeigbekehinde160@gmail.com>
2024-04-15 10:50:54 +0100

    trying to resolve the quantity default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_14_225451) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_14_230312) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
