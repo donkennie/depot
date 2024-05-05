916af5aafb3e974d782154cbe217ff31520c6288
donkennie <ajeigbekehinde160@gmail.com>
2024-05-05 21:39:18 +0100

    implement admin authentication

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,11 +17,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { name: @user.name, password: "secret", password_confirmation: "secret" } }
+      post users_url, params: { user: { name: 'sam',
+      password: "secret", password_confirmation: "secret" } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to users_url
   end
+
 
   test "should show user" do
     get user_url(@user)
@@ -34,9 +36,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { name: @user.name, password: "secret", password_confirmation: "secret" } }
-    assert_redirected_to user_url(@user)
+    patch user_url(@user), params: { user: { name: @user.name,
+      password: "secret", password_confirmation: "secret" } }
+    assert_redirected_to users_url
   end
+
 
   test "should destroy user" do
     assert_difference("User.count", -1) do
